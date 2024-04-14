59360d5b6ed11e3386a72dff127599e83cbedaa5
Jose Lopez <josejr7693@gmail.com>
2024-04-14 16:29:38 -0400

    Initial Commit

---
+++ Programming Practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Programming Practice
+//
+//  Created by Jose Lopez on 4/14/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
